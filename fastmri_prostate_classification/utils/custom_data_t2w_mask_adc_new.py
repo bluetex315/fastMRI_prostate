4aa92314576ba87dda378bb570506b6180b43746
Lihui <bluechen01@outlook.com>
2025-05-28 00:12:00 -0400

    supports real+syn training

--- fastmri_prostate_classification/utils/custom_data_t2w_mask_adc_new.py
+++ fastmri_prostate_classification/utils/custom_data_t2w_mask_adc_new.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn.functional as F
 import torch.utils.data as data
 from torch.utils.data.distributed import DistributedSampler
+from torch.utils.data import ConcatDataset
 from torch.autograd import Variable
 from torchvision.ops import sigmoid_focal_loss
 import random
@@ -54,325 +55,6 @@ class ProbabilisticScaleIntensity:
         return probabilistic_intensity_scaling(image, self.probability)
 
 
-# class FakeFastMRIDataset(data.Dataset):
-#     """
-#     Custom MONAI dictionary-based dataset for loading FastMRI prostate data at the slice level.
-
-#     Parameters:
-#     - datapath (str): Path to the folder containing patient data subfolders.
-#     - labelpath (str): Path to the folder containing label files.
-#     - norm_type (int): Integer representing the chosen normalization scheme.
-#     - augment (bool): Flag indicating whether to apply data augmentation.
-#     - split (str): One of 'train', 'val', or 'test' indicating the dataset split.
-#     """
-#     def __init__(self, config, datapath, labelpath, gland_maskpath, norm_type, augment, split):
-#         super().__init__()
-
-#         self.config = config
-#         self.datapath = datapath
-#         self.labelpath = labelpath
-#         self.gland_maskpath = gland_maskpath
-#         self.norm_type = norm_type
-#         self.augment = augment
-#         self.split = split
-        
-#         print(f"\n\n-------------------------- START Loading {self.split} Dataset --------------------------")
-#         print(f"LOADING ADC        --> {self.config['concat_adc']}")
-#         print(f"LOADING gland_mask --> {self.config['concat_mask']}")
-        
-#         # 1) get the exact same patient‐level splits
-#         self.train_ids, self.val_ids, self.test_ids = get_patient_splits(
-#             datapath=config['data']['real_datapath'],
-#             test_size=0.3,
-#             val_size=0.5,
-#             seed=config['seed'],
-#             exclude_ids=["223"]   # As patient_223 does not have correspoding segmentation, so remove it
-#         )
-
-#         print(f"[main] splits seed{config.get('seed', 42)}")
-#         print("[main] train ids\n", self.train_ids)
-#         print("[main] val ids\n", self.val_ids)
-#         print("[main] test ids\n", self.test_ids)
-#         print()
-#         print(f"Train: {len(self.train_ids)} patients")
-#         print(f"val:   {len(self.val_ids)} patients")
-#         print(f"test:  {len(self.test_ids)} patients")
-#         print()
-
-#         # # split off 70% train, 30% temp
-#         # train_ids, temp_ids = train_test_split(
-#         #     patient_ids,
-#         #     train_size=0.7,
-#         #     random_state=self.config['seed'],
-#         #     shuffle=True
-#         # )
-
-#         # # then split that 30% into equal val/test (0.15 each of original)
-#         # val_ids, test_ids = train_test_split(
-#         #     temp_ids,
-#         #     test_size=0.5,   # half of temp_ids → 0.15 of total
-#         #     random_state=self.config['seed'],
-#         #     shuffle=True
-#         # )
-
-#         if self.split == "train":
-#             patient_ids = self.train_ids
-#         elif self.split == "val":
-#             patient_ids = self.val_ids
-#         else:
-#             patient_ids = self.test_ids
-
-#         # load data
-#         dset_dict = {}
-#         if config['data']['real_datapath'] is not None:
-#             img_paths = [
-#                 os.path.join(root, file)
-#                 for root, _, files in sorted(os.walk(config['data']['real_datapath']))
-#                 for file in files if file.endswith("T2W.nii.gz")
-#             ]
-#             dset_dict["image"] = img_paths
-        
-        
-#         if config['data']['atlas_CGPZ_path'] is not None:
-#             seg_types = os.listdir(config['data']['atlas_CGPZ_path'])
-#             seg_paths = {
-#                 seg_type: [
-#                     os.path.join(root, file)
-#                     for root, _, files in sorted(os.walk(os.path.join(config['data']['atlas_CGPZ_path'], seg_type)))
-#                     for file in files if file.endswith(".nii.gz")
-#                 ]
-#                 for seg_type in seg_types
-#             }
-#             for seg_type in seg_types:
-#                 print(f"[main] Using {seg_type} for boundary slice removal")
-#                 seg_key = 'seg_' + seg_type
-#                 dset_dict.update({seg_key: seg_paths[seg_type]})
-
-#         print(f"[main] {dset_dict.keys()}")
-
-#         dset_dict_train = split_dset_by_patient(dset_dict, self.train_ids)
-#         dset_dict_val = split_dset_by_patient(dset_dict, self.val_ids)
-#         dset_dict_test = split_dset_by_patient(dset_dict, self.test_ids)
-
-#         if config['use_synthetic_data']:
-#             pass
-#         else:
-#             slices_dset_list_train = parse_3d_volumes(dset_dict_train, seg_type, label_csv_file=config['data']['label_csv_dir'])
-#             slices_dset_list_val = parse_3d_volumes(dset_dict_val, seg_type, label_csv_file=config['data']['label_csv_dir'])
-#             slices_dset_list_test = parse_3d_volumes(dset_dict_test, seg_type, label_csv_file=config['data']['label_csv_dir'])
-
-#         # pattern = re.compile(
-#         #     r'(?P<epoch>\d+)_'           
-#         #     r'(?P<patient>\d+)_slice'    
-#         #     r'(?P<slice>\d+)_'           
-#         #     r'(?P<kind>class|fakeclass)'
-#         #     r'(?P<label>\d+)_'           
-#         #     r'(?P<suffix>orig|syn)\.nii\.gz'
-#         # )     # naming pattern of fake images
-        
-#         # rows = []
-#         # NUM_CLASSES = 5
-
-#         # # 2) for each patient, list *only* the real (orig) class files first
-#         # for pt_id in sorted(patient_ids):
-#         #     pt_folder = os.path.join(datapath, f"patient_{pt_id}")
-#         #     if not os.path.isdir(pt_folder):
-#         #         continue
-
-#         # patient_dirs = [d for d in os.listdir(self.datapath) if d.startswith("patient_")]         # filter down to only the patient directories
-
-#         # Extract the numeric ID from each name
-#         patient_ids = []
-#         for d in patient_dirs:
-#             match = re.match(r"patient_(\d+)", d)
-#             if match:
-#                 patient_ids.append(match.group(1))
-
-#         for pt_id in patient_ids:       
-#             pt_folder = os.path.join(datapath, "patient_"+pt_id)
-#             if not os.path.isdir(pt_folder):
-#                 raise FileNotFoundError(f"Patient folder not found: {pt_folder}")
-            
-#             grouped = {}
-#             for path in os.listdir(pt_folder):      # iterate through patient folder, e.g `patient_078`
-#                 m = pattern.match(path)
-#                 if not m:
-#                     continue
-
-#                 slice_idx  = int(m.group('slice'))   # e.g. 1
-#                 kind       = m.group('kind')         # "class" or "fakeclass"
-#                 label      = int(m.group('label'))   # the numeric class
-#                 suffix     = m.group('suffix')       # "orig" or "syn"
-#                 fullpath   = os.path.join(pt_folder, path)
-                
-#                 # group key: one entry per (patient, slice)
-#                 key = (pt_id, slice_idx)
-#                 if key not in grouped:
-#                     grouped[key] = {
-#                         "patient_id":   pt_id,
-#                         "slice":        slice_idx,
-#                         "orig":         None,
-#                         "fake_classes": {},
-#                     }
-
-#                 entry = grouped[key]
-#                 if kind == "class" and suffix == "orig":
-#                     entry["orig"]  = fullpath
-#                     entry["label"] = label
-#                 elif kind == "fakeclass" and suffix == "syn":
-#                     entry.setdefault("fake_classes", {})[label] = fullpath
-
-#             # turn each grouped entry into a flat row
-#             for (pid, slice_idx), entry in grouped.items():
-#                 row = {
-#                     "patient_id": pid,
-#                     "slice_id":   slice_idx,
-#                     "orig":       entry["orig"],
-#                     "label":      entry["label"],
-#                 }
-#                 # now add fake_0…fake_4 (or whatever labels you have)
-#                 fake_dict = entry.get("fake_classes", {})
-#                 for lbl in range(5):  
-#                     row[f"fake_{lbl}"] = fake_dict.get(lbl, None)
-
-#                 rows.append(row)
-                
-#         self.df = pd.DataFrame(rows).sort_values(["patient_id","slice_id"], ignore_index=True)
-#         print(f"dataset line445 {self.split}dataset")
-#         print(self.df)
-#         print()
-
-#         print(f"dataset line446 {self.split}dataset real PI-RADS distribution")
-#         print(self.df['label'].value_counts().to_dict())
-
-#         # compute class-level weights for real data (weights computed by TRAINING only)
-#         if self.split == 'train':
-#             counts = self.df['label'].value_counts().sort_index().values.astype(np.float32)
-#             inv_counts = 1.0 / (counts + 1e-6)
-#             norm_weights = inv_counts / inv_counts.sum() * len(counts)
-#             self.class_weights = torch.tensor(norm_weights, dtype=torch.float)
-#             self.config['data']['train_class_weights'] = self.class_weights.tolist()
-#         else:
-#             w = self.config['data'].get('train_class_weights', None)
-#             if w is None:
-#                 raise ValueError('train_class_weights must be set in config for val/test splits')
-#             self.class_weights = torch.tensor(w, dtype=torch.float)
-        
-#         self.records = self.flatten_df()
-
-#         print(f"-------------------------- FINISH defining/loading {self.split} Dataset --------------------------\n\n")
-
-#         load_keys = ['image']
-
-#         # Define MONAI transforms
-#         if self.split == 'train': 
-            
-#             self.transforms = Compose([
-#                 LoadImaged(keys=load_keys),
-#                 EnsureChannelFirstd(keys=load_keys),
-#                 RandAffined(
-#                     keys=load_keys,
-#                     prob=0.5,
-#                     translate_range=(0, 16, 16)
-#                 ),
-#                 # ProbabilisticScaleIntensity(probability=0.5),
-#                 RandFlipd(
-#                     keys=load_keys,
-#                     prob=0.5, 
-#                     spatial_axis=1
-#                 ),
-#                 RandRotated(
-#                     keys=load_keys,
-#                     range_x=12,   # Degrees of rotation for the x-axis (between -12 and 12)
-#                     range_y=12,   # Degrees of rotation for the y-axis (between -12 and 12)
-#                     range_z=0.0,  # No rotation for the z-axis
-#                     prob=0.5,     # Ensure that the rotation is always applied
-#                     keep_size=True, # Keep the same image size after rotation (reshape=False equivalent)
-#                     padding_mode="zeros"
-#                 ),
-#                 # CenterSpatialCropd(keys=load_keys, roi_size=(224, 224)),
-#                 NormalizeIntensityd(keys=load_keys)
-#             ])
-
-#         else:
-#             self.transforms = Compose([
-#                 LoadImaged(keys=load_keys),
-#                 EnsureChannelFirstd(keys=load_keys),
-#                 # CenterSpatialCropd(keys=load_keys, roi_size=(224, 224)),
-#                 NormalizeIntensityd(keys=load_keys)
-#             ])
-
-#     def flatten_df(self):
-#         """
-#         Flatten each row into a list of dicts:
-#         - one for the original (paths in `orig`, label in `label`)
-#         - one for each fake_X path
-#         """
-#         records = []
-#         for _, row in self.df.iterrows():
-#             if self.split == "train" and self.config['data']['use_synthetic_data']:       # only append fake images/labels when training AND use_synthetic_data. For val/test, use true images/labels
-#                 # synthetic slices
-#                 for fake_lbl in range(5):
-#                     path = row[f"fake_{fake_lbl}"]
-#                     if path is not None:
-#                         records.append({
-#                             "patient_id":      row['patient_id'],
-#                             "slice_id":        row['slice_id'],
-#                             "orig_image_path": row["orig"],
-#                             "orig_label":      int(row["label"]),
-#                             "image":           path,        # fake_image_path --> image
-#                             "label":           fake_lbl     # fake_label --> label
-#                         })
-#             else:
-#                 records.append({
-#                     "patient_id":      row['patient_id'],
-#                     "slice_id":        row['slice_id'],
-#                     "image":           row["orig"],         # orig_image_path --> image
-#                     "label":           int(row["label"])    # orig_label --> label
-#                 })
-#         return records
-
-#     #  https://doi.org/10.1371/journal.pmed.1002699.s001 
-#     def loss(self, prediction, target):
-#         """
-#         Compute the weighted cross-entropy loss.
-
-#         Parameters:
-#         - prediction (Tensor): Model predictions.
-#         - target (Tensor): Ground truth labels.
-
-#         Returns:
-#         - nn.CrossEntropyLoss(): (Weighted) cross-entropy loss.
-#         """
-#         if self.split == 'train' and not self.config['data']['use_synthetic_data']:
-#             # use weighted CE
-#             weight = self.class_weights.to(prediction.device)
-#             criterion = torch.nn.CrossEntropyLoss(weight=weight)
-#             # print(f"[./utils/custom_data loss] using weighted CE {weight}")
-#             return criterion(prediction, target)
-#         else:
-#             # standard CE
-#             return torch.nn.CrossEntropyLoss()(prediction, target)
-         
-#     def __getitem__(self, index):
-        
-#         recs = self.records[index]
-
-#         transformed = self.transforms(recs)
-
-#         image = torch.FloatTensor(transformed['image'])
-#         label = torch.tensor(recs['label'], dtype=torch.long)
-        
-#         patient_id = recs['patient_id']
-#         slice_id = recs['slice_id']
-
-#         return image, label, patient_id, slice_id
-
-
-#     def __len__(self):
-#         return len(self.flatten_df())
-
-
 def save_images_from_loader(loader, save_dir, split,
                             n_images=16, saveims_format=('png',)):
     """
@@ -395,7 +77,7 @@ def save_images_from_loader(loader, save_dir, split,
     slice_ids = batch['slice_idx']
 
     images = images.detach().cpu()[:n_images]
-    print("line 398", images.shape)
+
     if images.ndim == 4 and images.size(1) > 1:
         images = images[:, :1, ...]  # keep only channel 0
 
@@ -558,6 +240,20 @@ def load_data(config, datapath, labelpath, gland_maskpath, norm_type, augment, s
         ToTensord(keys=tot_keys),
     ])
 
+
+
+    syn_slices_train_transforms = Compose([
+        # Add a channel dimension to 'image' and 'segm'
+        EnsureChannelFirstd(keys=tot_keys, channel_dim='no_channel'),
+        *cls_train_transforms.transforms,           # cls_train_transform (augmentation for cls) + ToTensor
+    ])
+
+    # if not using synthetic, then having diffusion transform, loading from original 3D data, need to match diffusion pipeline
+    real_slices_train_transforms = Compose([
+        *diffusion_pre_transforms.transforms,       # diffusion pipeline
+        *cls_train_transforms.transforms,           # cls_train_transform (augmentation for cls) + ToTensor
+    ])
+
     # eval transform for the downstream classification
     cls_eval_transforms = Compose([
         # EnsureChannelFirstd(keys=tot_keys, channel_dim='no_channel'),
@@ -570,33 +266,34 @@ def load_data(config, datapath, labelpath, gland_maskpath, norm_type, augment, s
         *cls_eval_transforms.transforms,            # cls_eval_transform + ToTensor
     ])
 
+
     if split == "train":
         if config['data']['use_synthetic_data']:
-            slices_dset_list_train = parse_syn_slices(train_ids, config['data']['syn_datapath'], split='train')
-            
-            # if using synthetic, then not having diffusion transform, loading from diffusion saved data
-            train_transforms = Compose([
-                # Add a channel dimension to 'image' and 'segm'
-                EnsureChannelFirstd(keys=tot_keys, channel_dim='no_channel'),
-                *cls_train_transforms.transforms,           # cls_train_transform (augmentation for cls) + ToTensor
-            ])
-        else:
-            slices_dset_list_train = parse_3d_volumes(dset_dict_train, seg_type, label_csv_file=config['data']['label_csv_dir'])
-            
-            # if not using synthetic, then having diffusion transform, loading from original 3D data, need to match diffusion pipeline
-            train_transforms = Compose([
-                *diffusion_pre_transforms.transforms,       # diffusion pipeline
-                *cls_train_transforms.transforms,           # cls_train_transform (augmentation for cls) + ToTensor
-            ])
+            syn_slices_dset_list_train = parse_syn_slices(train_ids, config['data']['syn_datapath'], split='train')
+            print("line 272", type(syn_slices_dset_list_train), syn_slices_dset_list_train[0])
+            syn_train_dataset = monai.data.Dataset(syn_slices_dset_list_train, transform=syn_slices_train_transforms)
 
+            if config['data']['combine_real_data']:
+                real_slices_dset_list_train = parse_3d_volumes(dset_dict_train, seg_type, label_csv_file=config['data']['label_csv_dir'])
+                print("line 277", type(real_slices_dset_list_train), real_slices_dset_list_train[0])
+                real_train_dataset = monai.data.Dataset(real_slices_dset_list_train, transform=real_slices_train_transforms)
+                
+                train_dataset = ConcatDataset([syn_train_dataset, real_train_dataset])
+
+            else:
+                train_dataset = syn_train_dataset
+            
+        else:       # using only real data extracted from 3D volume
+            real_slices_dset_list_train = parse_3d_volumes(dset_dict_train, seg_type, label_csv_file=config['data']['label_csv_dir'])
+            train_dataset = real_train_dataset
+
+        # validation set will be all real images regardless
         slices_dset_list_val = parse_3d_volumes(dset_dict_val, seg_type, label_csv_file=config['data']['label_csv_dir'])
-
-        train_dataset = monai.data.Dataset(slices_dset_list_train, transform=train_transforms)
         val_dataset = monai.data.Dataset(slices_dset_list_val, transform=eval_transforms)
 
         train_loader = DataLoader(train_dataset, batch_size=128, num_workers=4, shuffle=True)
         val_loader = DataLoader(val_dataset, batch_size=128, num_workers=0, shuffle=False)
-        
+
         # optionally save some samples
         if saveims and rank == 0:
             base_dir = os.path.join(rundir, "sample_images")
--- fastmri_prostate_classification/utils/utils.py
+++ fastmri_prostate_classification/utils/utils.py
@@ -37,7 +37,7 @@ def parse_syn_slices(
     """
     Build a list of dicts, each containing:
       - "image":   full path to the .nii.gz
-      - "label":   int class 0–4
+      - "label":   int class 0-4
       - "patient_id": str patient identifier (e.g. "001")
       - "slice_idx":  int slice number (parsed from "_slice{n}_")
     
@@ -171,23 +171,24 @@ def parse_3d_volumes(dset_dict, seg_type, label_csv_file=None):
                 'patient_id': patient_id,
                 'slice_idx': z,
                 'image': img_vol[:, :, z],
-                seg_key: seg_vol[:, :, z]
+                # seg_key: seg_vol[:, :, z]
             }
 
-            # Get the left (previous) slice or pad if at boundary.
-            if z - 1 >= 0:
-                clean_left = img_vol[:, :, z - 1]
-            else:
-                clean_left = np.zeros(img_vol[:, :, z].shape, dtype=img_vol.dtype)
+            # @deprecated as no need for clean_left, clean_right, and seg_key
+            # # Get the left (previous) slice or pad if at boundary.
+            # if z - 1 >= 0:
+            #     clean_left = img_vol[:, :, z - 1]
+            # else:
+            #     clean_left = np.zeros(img_vol[:, :, z].shape, dtype=img_vol.dtype)
 
-            # Get the right (next) slice or pad if at boundary.
-            if z + 1 < num_slices:
-                clean_right = img_vol[:, :, z + 1]
-            else:
-                clean_right = np.zeros(img_vol[:, :, z].shape, dtype=img_vol.dtype)
+            # # Get the right (next) slice or pad if at boundary.
+            # if z + 1 < num_slices:
+            #     clean_right = img_vol[:, :, z + 1]
+            # else:
+            #     clean_right = np.zeros(img_vol[:, :, z].shape, dtype=img_vol.dtype)
 
-            slice_info['clean_left'] = clean_left
-            slice_info['clean_right'] = clean_right
+            # slice_info['clean_left'] = clean_left
+            # slice_info['clean_right'] = clean_right
             
             # Add the slice-level class label if available.
             key = (patient_id, z)
